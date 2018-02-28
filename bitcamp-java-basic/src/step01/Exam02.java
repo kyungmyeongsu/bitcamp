0a92bac5aed11fc64454d988062bd9a3b8e38601
kyungmyeongsu <oplu92@gmail.com>
2018-02-28 17:22:44 +0900

    'test'

---
+++ bitcamp-java-basic/src/step01/Exam02.java
@@ -0,0 +1,6 @@
+package step01;
+public class Exam02 {
+    public static void main(String[] args) {
+        System.out.println("src/step01/Exam02");
+    }
+}
---
+++ bitcamp-java-basic/src/step01/Exam03.java
@@ -0,0 +1,11 @@
+package step01;
+public class Exam03 {
+    public static void main(String[] args) {
+        System.out.println("src/step01/Exam03");
+    }
+
+    @Override
+    public String toString(){
+        return "Exam03";
+    }
+}
