32697c99ded0f5e7090a752f4f056b6b48029594
myeongsu <oplu92@gmail.com>
2018-04-05 17:30:29 +0900

    test

---
+++ bitcamp-java-basic/src/step18/ex6/Exam04.java
@@ -0,0 +1,43 @@
+// 응용 - 특정 패키지 및 그 하위 패키지의 .class 파일 목록을 알아내기
+package step18.ex6;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam04 {
+    public static void main(String[] args) throws Exception{
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        URL url = classLoader.getResource("step18");
+        
+        List<File> files = findClasses(new File(url.getPath()));
+        
+        for (File f : files) {
+            System.out.println(f.getCanonicalPath());
+        }
+    }
+    
+    static List<File> findClasses(File dir) throws Exception {
+        File[] files =  dir.listFiles(new FileFilter() {
+            
+            @Override
+            public boolean accept(File file) {
+                if (file.isDirectory() || (file.getName().endsWith(".class") && !file.getName().contains("$")))
+                    return true;
+                return false;
+            }
+        });
+        
+        ArrayList<File> list = new ArrayList<>();
+        for (File f : files) {
+            if (f.isDirectory()) {
+                list.addAll(findClasses(f));
+            } else {
+                list.add(f);
+            }
+        }
+        return list;
+    }
+}
