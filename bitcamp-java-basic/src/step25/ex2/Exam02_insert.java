503634043bf84281caa283be888b1f6b98bc7a59
myeongsu <oplu92@gmail.com>
2018-04-23 17:34:09 +0900

    test

--- bitcamp-java-basic/src/step25/ex2/Exam02_insert.java
+++ bitcamp-java-basic/src/step25/ex2/Exam02_insert.java
@@ -2,7 +2,7 @@ package step25.ex2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Exam02_insert {
@@ -15,20 +15,23 @@ public class Exam02_insert {
                 "jdbc:mysql://localhost:3306/java106db?serverTimezone=UTC&useSSL=false",
                 "java106","1111");
         
-        Statement stmt = con.createStatement();
-        
         System.out.print("제목? ");
         String title = keyScan.nextLine();
         
         System.out.print("내용? ");
         String content = keyScan.nextLine();
         
-        String sql = String.format(
-                "insert into ex_board(titl,cont,rdt) values('%s','%s',now())",
-                title, content);
+        // 값이 들어갈 자리에 in-parameter(?)를 지정한다.
+        // => 데이터 타입에 상관없이 ?를 넣는다.
+        PreparedStatement stmt = con.prepareStatement(
+                "insert into ex_board(titl,cont,rdt) values(?,?,now())");
         
+        // in-parameter에 값을 설정한다.
+        // => 설정하는 순서는 상관없다.
+        stmt.setString(2, content);
+        stmt.setString(1, title);
         
-        int count = stmt.executeUpdate(sql);
+        int count = stmt.executeUpdate();
         System.out.printf("%d 개 입력 성공!", count);
         
         stmt.close();
