946721adf43d60ddeb9a561cca1b218520df37c4
myeongsu <oplu92@gmail.com>
2018-05-08 08:58:23 +0900

    test

---
+++ spring-ioc/src/main/java/bitcamp/java106/step06/CarFactory3.java
@@ -0,0 +1,52 @@
+package bitcamp.java106.step06;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class CarFactory3 implements FactoryBean<Car> {
+    String model;
+    
+    public CarFactory3() {
+        System.out.println("CarFactory3() 호출됨.");
+    }
+    
+    public void setModel(String model) {
+        System.out.println("CarFactory3.setModel() 호출됨.");
+        this.model = model;
+    }
+    
+    @Override
+    public Car getObject() throws Exception {
+        System.out.println("CarFactory3.getObject() 호출됨.");
+        Car c = new Car();
+        switch (model) {
+        case "티코":
+            c.setMaker("대우자동차");
+            c.setModel("Tico");
+            c.setCc(890);
+            return c;
+        case "소나타":
+            c.setMaker("현대자동차");
+            c.setModel("Sonata");
+            c.setCc(1980);
+            return c;
+        case "SM5":
+            c.setMaker("르노삼성자동차");
+            c.setModel("SM5");
+            c.setCc(1990);
+            return c;
+        default:
+            c.setMaker("비트자동차");
+            c.setModel("자바휘웅");
+            c.setCc(5000);
+            return c;
+        }
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        System.out.println("CarFactory3.getObjectType() 호출됨.");
+        return Car.class;
+    }
+    
+    
+}
---
+++ spring-ioc/src/main/java/bitcamp/java106/step06/Exam05.java
@@ -0,0 +1,20 @@
+// 팩토리 메서드 호출
+package bitcamp.java106.step06;
+
+import java.sql.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam05 {
+
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step06/application-context-05.xml");
+        
+        // 이 예제는 Exam04와 같다.
+        // 다만 Factory 클래스릐 이름을 CarFactory3에서 CarFactoryBean으로 바꾼 것이다.
+        System.out.println(iocContainer.getBean("c1"));
+    }
+
+}
