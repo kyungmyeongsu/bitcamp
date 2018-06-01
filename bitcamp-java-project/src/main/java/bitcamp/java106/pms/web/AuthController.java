1864e96cbd1c2390d334f1016098ecde9380669b
myeongsu <oplu92@gmail.com>
2018-06-01 09:58:04 +0900

    test

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/web/AuthController.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/web/AuthController.java
@@ -8,12 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import bitcamp.java106.pms.dao.MemberDao;
 import bitcamp.java106.pms.domain.Member;
 
-@Component("/auth")
+@Controller
+@RequestMapping("/auth")
 public class AuthController {
     
     MemberDao memberDao;
@@ -27,7 +30,7 @@ public class AuthController {
     public String login(
             @RequestParam("id") String id,
             @RequestParam("password") String password,
-            @RequestParam("saveId") String saveId,
+            @RequestParam(value="saveId",required=false) String saveId,
             HttpServletResponse response,
             HttpServletRequest request,
             HttpSession session) throws Exception {
@@ -58,9 +61,11 @@ public class AuthController {
                 // 로그인 하기 전의 페이지로 이동한다.
                 String refererUrl = (String)session.getAttribute("refererUrl");
                 
-                if (refererUrl == null) { 
+                if (refererUrl == null || 
+                    refererUrl.contains("login.do") ||
+                    refererUrl.endsWith("/auth/form.jsp")) { 
                     // 이전 페이지가 없다면 메인 화면으로 이동시킨다.
-                    return "redirect:" + request.getContextPath(); // => "/java106-java-project"
+                    return "redirect:/"; // => "/java106-java-project"
                 } else { 
                     // 이전 페이지가 있다면 그 페이지로 이동시킨다.
                     return "redirect:" + refererUrl;
@@ -81,7 +86,7 @@ public class AuthController {
         session.invalidate();
         
         // 웹 애플리케이션의 시작 페이지로 가라고 웹브라우저에게 얘기한다.
-        return "redirect:" + request.getContextPath();
+        return "redirect:/";
     }
 }
 
