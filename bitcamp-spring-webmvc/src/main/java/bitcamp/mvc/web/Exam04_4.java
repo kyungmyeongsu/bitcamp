ec7e0b009c45a1c84aa48f06e6ef30aa22c1d38b
myeongsu <oplu92@gmail.com>
2018-06-04 15:53:34 +0900

    test

---
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam04_4.java
@@ -0,0 +1,58 @@
+// @RequestMapping - HTTP 헤더로 메서드 구분하기
+package bitcamp.mvc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+@RequestMapping("/exam04_4") 
+public class Exam04_4 {
+    
+    // HTTP 요청에서 "Content-Type" 헤더의 값에 따라 호출될 메서드를 구분하기
+    // => 클라이언트가 POST 요청으로 데이터를 보낼때 
+    //    서버에 그 데이터가 무슨 타입인지 알려줘야 한다.
+    //    그 때 사용하는 헤더가  "Content=Type"이다.
+    // 테스트 방법: 
+    // 예1) exam04_4.html 
+    
+// POST 요청
+/*
+POST /java106-spring-webmvc/mvc/exam04_4/get HTTP/1.1
+Host: localhost:8888
+Accept: ...
+User-Agent: ...
+Content-Type: 
+
+name=aaa&age=20&tel=2222-2222&postno=19876&addr=서울시
+*/
+    
+    // 만약 클라이언트가 보낸 데이터가 "변수=값&변수=값&변수=값" 형태로 돼 있다면?
+    @PostMapping(value="post", consumes="application/x-www-form-urlencoded")  
+    @ResponseBody  
+    public String m1() {
+        return "Exam04_4.m1()";
+    }
+    
+    // 만약 클라이언트가 보낸 데이터가 "값,값,값,값" 형태로 돼 있다면?
+    @PostMapping(value="post", produces="text/csv")  
+    @ResponseBody  
+    public String m2() {
+        return "Exam04_4.m2()";
+    }
+    
+    // 만약 클라이언트가 보낸 데이터가 {"name":"aaa","age":20,"tel":"1111-2222"} 형태로 돼 있다면?
+    @PostMapping(value="post", produces="application/json")  
+    @ResponseBody  
+    public String m3() {
+        return "Exam04_4.m3()";
+    }
+}
+
+
+
+
+
+
+
