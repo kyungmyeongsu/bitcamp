585950c9023c2165106f71436596c13d84babcb0
myeongsu <oplu92@gmail.com>
2018-06-08 16:38:25 +0900

    test

--- spring-ioc/src/main/java/bitcamp/java106/step13/ex5/X.java
+++ spring-ioc/src/main/java/bitcamp/java106/step13/ex5/X.java
@@ -1,10 +1,12 @@
-package bitcamp.java106.step13.ex2;
+package bitcamp.java106.step13.ex5;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class X {
-    public void m1() {
+    public int m1(int a, int b) {
         System.out.println("X.m1()");
+        int result = a / b;
+        return result;
     }
 }
