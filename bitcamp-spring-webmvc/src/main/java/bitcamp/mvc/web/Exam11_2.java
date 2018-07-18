b8696ad4fba58d8cf03791e5707c76f75121e4f6
myeongsu <oplu92@gmail.com>
2018-07-18 18:04:01 +0900

    test

---
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam11_2.java
@@ -0,0 +1,108 @@
+// 파일 업로드 - JSON
+package bitcamp.mvc.web;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController 
+@RequestMapping("/exam11_2") 
+public class Exam11_2 {
+    
+    @Autowired ServletContext sc;
+    
+    @PostMapping("upload01")
+    public Object upload01(
+            MultipartFile files) {
+        
+        HashMap<String,Object> jsonData = new HashMap<>();
+        
+        String filesDir = sc.getRealPath("/files");
+        
+        String filename = UUID.randomUUID().toString();
+        jsonData.put("filename", filename);
+        jsonData.put("filesize", files.getSize());
+        jsonData.put("originname", files.getOriginalFilename());
+        try {
+            File path = new File(filesDir + "/" + filename);
+            System.out.println(path);
+            files.transferTo(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonData;
+    }
+    
+    @PostMapping("upload02")
+    public Object upload02(
+            MultipartFile[] files) {
+        
+        ArrayList<Map<String,Object>> jsonDataList = new ArrayList<>();
+        String filesDir = sc.getRealPath("/files");
+        
+        for (int i = 0; i < files.length; i++) {
+            HashMap<String,Object> jsonData = new HashMap<>();
+            String filename = UUID.randomUUID().toString();
+            jsonData.put("filename", filename);
+            jsonData.put("filesize", files[i].getSize());
+            jsonData.put("originname", files[i].getOriginalFilename());
+            try {
+                File path = new File(filesDir + "/" + filename);
+                System.out.println(path);
+                files[i].transferTo(path);
+                jsonDataList.add(jsonData);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonDataList;
+    }
+    
+    
+    @PostMapping("upload03")
+    public Object upload03(String name, int age, MultipartFile[] files) {
+        
+        String filesDir = sc.getRealPath("/files");
+        
+        HashMap<String,Object> returnData = new HashMap<>();
+        returnData.put("name", name);
+        returnData.put("age", age);
+        
+        ArrayList<Map<String,Object>> jsonDataList = new ArrayList<>();
+        returnData.put("files", jsonDataList);
+        
+        for (int i = 0; i < files.length; i++) {
+            HashMap<String,Object> jsonData = new HashMap<>();
+            String filename = UUID.randomUUID().toString();
+            jsonData.put("filename", filename);
+            jsonData.put("filesize", files[i].getSize());
+            jsonData.put("originname", files[i].getOriginalFilename());
+            try {
+                File path = new File(filesDir + "/" + filename);
+                System.out.println(path);
+                files[i].transferTo(path);
+                jsonDataList.add(jsonData);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return returnData;
+    }
+}
+
+
+
+
+
+
+
