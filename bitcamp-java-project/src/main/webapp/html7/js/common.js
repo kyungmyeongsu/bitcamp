ab6fd3d9aeff7d8fd71d37893bd7a6486626b809
myeongsu <oplu92@gmail.com>
2018-08-16 15:59:56 +0900

    '.'

--- bitcamp-java-project/src/main/webapp/html7/js/common.js
+++ bitcamp-java-project/src/main/webapp/html7/js/common.js
@@ -1,1 +1,2 @@
-var serverRoot = "http://localhost:8888/bitcamp-java-project";
+//var serverRoot = "http://localhost:8888/bitcamp-java-project";
+var serverRoot = "http://www.kyungmyeongus.com:8888/bitcamp-java-project";
